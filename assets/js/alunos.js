0f34df2be7a9a0e9ccbdbf90591ee591d17fdcd2
Igor César <igorcesar.dev@gmail.com>
2022-04-28 22:04:09 -0300

    create_new_repository-SysBoletim

---
+++ assets/js/alunos.js
@@ -0,0 +1,10 @@
+const alunos = [{
+    nome: "Igor César da Silva",
+    matricula: 1619067,
+    curso: "Operador de Computador",
+},
+{
+    nome: "Mateus Froes",
+    matricula: 1619068,
+    curso: "Designer Gráfico",
+}];
