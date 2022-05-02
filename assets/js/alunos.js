0ed8a24a64cdb9b0133d40f4a039bf213798a100
Igor César <igorcesar.dev@gmail.com>
2022-05-02 12:56:35 -0300

    create_script_login_and_other_updates

--- assets/js/alunos.js
+++ assets/js/alunos.js
@@ -1,16 +1,35 @@
 const alunos = [{
     nome: "Igor César da Silva",
     matricula: 1619067,
+    telefone: 991711242,
     curso: "Operador de Computador",
-},
-{
-    nome: "Mateus Froes",
-    matricula: 1619068,
-    curso: "Designer Gráfico",
 }];
 
-const avaliacoes = [{
-    matricula: 1619067,
-    modulo: "Introdução à Informática",
-    data: "10/05/2022",
-}];
+
+function exibirAluno(){
+    var containerAluno = document.getElementById('aluno');
+    alunos.map((value) => {
+        containerAluno.innerHTML +=  `
+        <div class="box-right-head">
+            <p class="name-student">Nome: `+value.nome+`</p>
+            <p class="code-student">Matrícula: `+value.matricula+`</p>
+            <p class="phone-student">Telefone: `+value.telefone+`</p>
+        </div>
+        `
+    })
+}
+exibirAluno();
+
+
+function exibirAvaliação(){
+    var containerAvaliacao = document.getElementById('avaliacao');
+    alunos.map((value) => {
+        containerAvaliacao.innerHTML += `
+        <div class="box-right-contents">
+            <p class="exam-student border">Avaliações</p>
+            <p class="exam-date border">Data da Avaliação</p>
+            <p class="exam-note border">Notas</p>
+        </div>
+        `
+    })
+}
